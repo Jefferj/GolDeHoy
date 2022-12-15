ba2153e4947b54216501ed2ec79bfcccf15ee80d
Jefferj <jefferj080@gmail.com>
2022-12-15 16:04:36 -0500

    LoginView

---
+++ GolDeHoy/Modules/Register/ViewModel/RegisterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterViewController.swift
+//  GolDeHoy
+//
+//  Created by Jefferson Naranjo rodríguez on 15/12/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var imageProfile: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textfielName: UITextField!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var textfieldEmail: UITextField!
+    @IBOutlet weak var labelEmail: UILabel!
+    @IBOutlet weak var texfieldPassword: UITextField!
+    @IBOutlet weak var labelPassword: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func buttonRegister(_ sender: Any) {
+    }
+}
