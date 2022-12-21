2934f78798e6bf4ab51891986206f48aab970d65
Jefferj <jefferj080@gmail.com>
2022-12-21 16:41:10 -0500

    add views tabBar

--- GolDeHoy/Modules/Home/ViewModel/HomeViewController.swift
+++ GolDeHoy/Modules/Home/ViewModel/HomeViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var tableViewHome: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
